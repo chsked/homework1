860b36df8bd3ae6856776d2c4fcd4db4755a5bd3
chsked <sthsanaa123@gmail.com>
2024-07-08 10:25:18 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+//        //wap to find the sum of 2 numbers taking input
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the first number:");
+//        int num1 = sc.nextInt();
+//        System.out.println("Enter the second number:");
+//        int num2 = sc.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println("The sum is: " +sum);
+//
+        //not taking input
+//        //int a = 5, b = 6; easy way
+//        int a = 5;
+//        int b = 6;
+//        int sum = a + b;
+//        System.out.println("The sum is" +sum);
+//
+//        // wap to find area of circle taking input
+//        double r = 20;
+//        double pie = 3.14;
+//        double circle = (r*r)*pie;
+//        System.out.println("The circle is" +circle);
+
+//        //not taking input
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a radius of a circle:");
+//        int radius = sc.nextInt();
+//        double pie = 3.14;
+//        double area = pie *(radius * radius);
+//        System.out.println("The area of the circle is " + area);
+
+
+//
+//
+//        // wap to check if the num is odd or even taking input
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number:");
+//        int num = sc.nextInt();
+//        if(num%2==0) {
+//            System.out.println(num + " is even");
+//        }
+//        else{
+//            System.out.println(num + "is odd");
+//        }
+        // not taking input
+//        int num = 15;
+//        if(num%2==0) {
+//            System.out.println(num + " is even");
+//        }
+//        else {
+//            System.out.println(num + " is odd");
+//        }
+//
+
+    }
+}
+
